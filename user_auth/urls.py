18d3762807ba2046756e05add5cd1d5e1be3f98a
Tamaki_plana <naokillme12212000@gmail.com>
2020-04-09 11:03:05 +0900

    SNS認証の実装

---
+++ user_auth/urls.py
@@ -0,0 +1,16 @@
+import django.contrib.auth.views
+from django.urls import path
+
+from user_auth import views
+
+app_name = 'user_auth'
+
+urlpatterns = [
+    path('top/', views.top_page, name="top"),  # リダイレクト
+    path('login/',  # ログイン
+         django.contrib.auth.views.LoginView.as_view(template_name='user_auth/login.html'),
+         name='login'),
+    path('logout/',  # ログアウト
+         django.contrib.auth.views.LogoutView.as_view(template_name='user_auth/login.html'),
+         name='logout'),
+]
---
+++ user_auth/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from social_django.models import UserSocialAuth
+
+
+@login_required
+def top_page(request):
+    user = UserSocialAuth.objects.get(user_id=request.user.id)
+    return render(request, 'user_auth/top.html', {'user': user})
