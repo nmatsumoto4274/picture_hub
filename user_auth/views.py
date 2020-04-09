7bdc7fc5e1a2a8def37600e2e51e35ace5f4c4a7
Tamaki_plana <naokillme12212000@gmail.com>
2020-04-09 14:50:26 +0900

    とりあえず自分のTLを取得するようお試し実装

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,11 +1,22 @@
-from django.contrib.auth.decorators import login_required
+import twitter
 from django.shortcuts import render
 
 from social_django.models import UserSocialAuth
 
+from picture_hub import settings
+
 
 def top_page(request):
     user = None
+    tweet = None
     if request.user.id is not None:
         user = UserSocialAuth.objects.get(user_id=request.user.id)
-    return render(request, 'user_auth/top.html', {'user': user})
+        # ツイートの取得
+        api = twitter.Api(
+            consumer_key=settings.SOCIAL_AUTH_TWITTER_KEY,
+            consumer_secret=settings.SOCIAL_AUTH_TWITTER_SECRET,
+            access_token_key=user.access_token['oauth_token'],
+            access_token_secret=user.access_token['oauth_token_secret']
+        )
+        tweet = api.GetHomeTimeline()
+    return render(request, 'user_auth/top.html', {'user': user, 'tweet': tweet})
