e544cb79161ad8abec209591b32b1252b87b296a
Tamaki_plana <naokillme12212000@gmail.com>
2020-04-09 11:43:46 +0900

    ログイン画面を廃止し、トップ画面に集約。

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -7,10 +7,7 @@ app_name = 'user_auth'
 
 urlpatterns = [
     path('top/', views.top_page, name="top"),  # リダイレクト
-    path('login/',  # ログイン
-         django.contrib.auth.views.LoginView.as_view(template_name='user_auth/login.html'),
-         name='login'),
     path('logout/',  # ログアウト
-         django.contrib.auth.views.LogoutView.as_view(template_name='user_auth/login.html'),
+         django.contrib.auth.views.LogoutView.as_view(template_name='user_auth/logout.html'),
          name='logout'),
 ]
