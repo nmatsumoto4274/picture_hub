b9bc7c115a1f22f49b826f7c7d1d300f9ea8956d
Tamaki_plana <naokillme12212000@gmail.com>
2020-04-26 10:51:31 +0900

    Tweetの検索まで実装

--- tweet_crawl/management/commands/crawler.py
+++ tweet_crawl/management/commands/crawler.py
@@ -1,8 +1,10 @@
 import twitter
+from django.db.models import Max
 from social_django.models import UserSocialAuth
 from django.core.management import BaseCommand
 
 from picture_hub import settings
+from tweet_crawl.models import Tweet
 
 
 class Command(BaseCommand):
@@ -11,26 +13,28 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **kwargs):
-        # TODO DBに登録されたユーザの情報を取得し、新たなハッシュタグ・画像付きのツイートが存在するかチェックします。
         # ユーザ情報取得
         users = UserSocialAuth.objects.all()
 
         for user in users:
-            # 必要なユーザ情報を取り出し
             uid = user.uid
             token_dict = user.extra_data['access_token']
-
             api = twitter.Api(consumer_key=settings.SOCIAL_AUTH_TWITTER_KEY,
                               consumer_secret=settings.SOCIAL_AUTH_TWITTER_SECRET,
                               access_token_key=token_dict['oauth_token'],
                               access_token_secret=token_dict['oauth_token_secret'])
-
-            # uidに紐づくユーザ情報取得
             twitter_user = api.GetUser(user_id=uid)
-            screen_name = twitter_user.screen_names
 
-            # 検索に必要なツイート情報を取得
+            screen_name = twitter_user.screen_name
+            since_id = Tweet.objects.all().aggregate(Max('id'))
+
+            # ツイートの検索
+            if since_id is None:
+                tweets = api.GetSearch(term='#Pict_Hub -RT filter:images from:' + screen_name)
+            else:
+                tweets = api.GetSearch(term='#Pict_Hub -RT filter:images from:' + screen_name,
+                                       since_id=since_id['id__max'])
 
             # 登録未済のツイートが存在していたら、取得して登録
-
-            print(twitter_user.screen_name)
+            for tweet in tweets:
+                print(twitter_user.screen_name, since_id, tweets)
