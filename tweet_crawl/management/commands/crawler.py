e779467b9d204461f7027a906a280aeb5dc53f01
Tamaki_plana <naokillme12212000@gmail.com>
2020-04-28 10:30:24 +0900

    テーブル更新まで実装

--- tweet_crawl/management/commands/crawler.py
+++ tweet_crawl/management/commands/crawler.py
@@ -1,13 +1,29 @@
+import calendar
+import time
+
 import twitter
+from django.core.management import BaseCommand
 from django.db.models import Max
 from social_django.models import UserSocialAuth
-from django.core.management import BaseCommand
 
 from picture_hub import settings
-from tweet_crawl.models import Tweet
+from tweet_crawl.models import Tweet, Picture
 
 
 class Command(BaseCommand):
+
+    def __conv_time(self, created_at):
+        """
+        Twitter のcreated_at をDATETIMEで扱える形に変換する。
+        :param created_at:
+        :return:
+        """
+        time_utc = time.strptime(created_at, '%a %b %d %H:%M:%S +0000 %Y')
+        unix_time = calendar.timegm(time_utc)
+        time_local = time.localtime(unix_time)
+        japan_time = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
+        return japan_time
+
     """
     ユーザの画像付きツイートを収集するコマンド
     """
@@ -17,13 +33,14 @@ class Command(BaseCommand):
         users = UserSocialAuth.objects.all()
 
         for user in users:
-            uid = user.uid
+            user_id = user.id
+            user_uid = user.uid
             token_dict = user.extra_data['access_token']
             api = twitter.Api(consumer_key=settings.SOCIAL_AUTH_TWITTER_KEY,
                               consumer_secret=settings.SOCIAL_AUTH_TWITTER_SECRET,
                               access_token_key=token_dict['oauth_token'],
                               access_token_secret=token_dict['oauth_token_secret'])
-            twitter_user = api.GetUser(user_id=uid)
+            twitter_user = api.GetUser(user_id=user_uid)
 
             screen_name = twitter_user.screen_name
             since_id = Tweet.objects.all().aggregate(Max('id'))
@@ -35,8 +52,22 @@ class Command(BaseCommand):
                 tweets = api.GetSearch(term='#Pict_Hub -RT filter:images from:' + screen_name,
                                        since_id=since_id['id__max'])
 
-            # TODO 登録未済のツイートだった場合、画像を登録
-
-            # TODO 登録未済のツイートを画像と紐づけて登録
             for tweet in tweets:
-                print(twitter_user.screen_name, since_id, tweets)
+                if Tweet.objects.filter(id=tweet.id).count() >= 0:
+                    t = Tweet(
+                        id=tweet.id,
+                        user_id=user_id,
+                        create_datetime=self.__conv_time(tweet.created_at)
+                    )
+                    t.save()
+
+                    # 画像の登録
+                    for picture in tweet.media:
+                        p = Picture(
+                            id=picture.id,
+                            tweet_id=tweet.id,
+                            picture_url=picture.media_url_https
+                        )
+                        p.save()
+
+            print(twitter_user.screen_name, 'Done!')
--- tweet_crawl/models.py
+++ tweet_crawl/models.py
@@ -6,13 +6,15 @@ class Tweet(models.Model):
     """
     ツイート
     """
+    id = models.BigIntegerField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    create_datetime = models.DateField()
+    create_datetime = models.DateTimeField()
 
 
 class Picture(models.Model):
     """
     写真
     """
+    id = models.BigIntegerField(primary_key=True)
     tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
     picture_url = models.URLField(max_length=200)
