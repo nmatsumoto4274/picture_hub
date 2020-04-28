b27279b4d024d40f459055760cf0926231c16408
Tamaki_plana <naokillme12212000@gmail.com>
2020-04-28 09:35:41 +0900

    TweetモデルとPictureモデルが1対多となるよう修正

--- tweet_crawl/models.py
+++ tweet_crawl/models.py
@@ -2,18 +2,17 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Picture(models.Model):
-    """
-    写真
-    """
-    tweet_id = models.IntegerField(primary_key=True)
-    picture_url = models.URLField(max_length=200)
-
-
 class Tweet(models.Model):
     """
     ツイート
     """
-    pictures = models.ForeignKey(Picture, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     create_datetime = models.DateField()
+
+
+class Picture(models.Model):
+    """
+    写真
+    """
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    picture_url = models.URLField(max_length=200)
