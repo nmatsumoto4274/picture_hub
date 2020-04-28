d991ad8abd1dfc6289b2b81d781028891a502505
Tamaki_plana <naokillme12212000@gmail.com>
2020-04-28 11:05:03 +0900

    crawlerコマンドのリファクタリング実施

--- tweet_crawl/management/commands/crawler.py
+++ tweet_crawl/management/commands/crawler.py
@@ -11,6 +11,9 @@ from tweet_crawl.models import Tweet, Picture
 
 
 class Command(BaseCommand):
+    """
+    ユーザの画像付きツイートを収集するコマンド
+    """
 
     def __conv_time(self, created_at):
         """
@@ -24,50 +27,41 @@ class Command(BaseCommand):
         japan_time = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
         return japan_time
 
-    """
-    ユーザの画像付きツイートを収集するコマンド
-    """
+    def __get_tweets(self, api, screen_name, since_id):
+        """
+        ツイートの取得
+        :param api: Twitter APIのインスタンス
+        :param screen_name: Twitter表示名
+        :param since_id: Tweetテーブルに存在するレコードのID最大値
+        :return: Tweetリスト
+        """
+        term = '#Pict_Hub -RT filter:images from:' + screen_name
+        return api.GetSearch(term=term) if since_id is None else api.GetSearch(term=term, since_id=since_id['id__max'])
 
     def handle(self, *args, **kwargs):
-        # ユーザ情報取得
         users = UserSocialAuth.objects.all()
-
         for user in users:
-            user_id = user.id
-            user_uid = user.uid
+            # Twitter APIインスタンスの生成
             token_dict = user.extra_data['access_token']
             api = twitter.Api(consumer_key=settings.SOCIAL_AUTH_TWITTER_KEY,
                               consumer_secret=settings.SOCIAL_AUTH_TWITTER_SECRET,
                               access_token_key=token_dict['oauth_token'],
                               access_token_secret=token_dict['oauth_token_secret'])
-            twitter_user = api.GetUser(user_id=user_uid)
 
-            screen_name = twitter_user.screen_name
-            since_id = Tweet.objects.all().aggregate(Max('id'))
+            # Twitter APIで取得した最新のユーザ情報を正として利用する
+            twitter_user = api.GetUser(user_id=user.uid)
 
-            # ツイートの検索
-            if since_id is None:
-                tweets = api.GetSearch(term='#Pict_Hub -RT filter:images from:' + screen_name)
-            else:
-                tweets = api.GetSearch(term='#Pict_Hub -RT filter:images from:' + screen_name,
-                                       since_id=since_id['id__max'])
+            # 未登録ツイートの検索・取得
+            tweets = self.__get_tweets(api, twitter_user.screen_name, Tweet.objects.all().aggregate(Max('id')))
 
+            # 登録処理
             for tweet in tweets:
                 if Tweet.objects.filter(id=tweet.id).count() >= 0:
-                    t = Tweet(
-                        id=tweet.id,
-                        user_id=user_id,
-                        create_datetime=self.__conv_time(tweet.created_at)
-                    )
+                    t = Tweet(id=tweet.id, user_id=user.id, create_datetime=self.__conv_time(tweet.created_at))
                     t.save()
-
-                    # 画像の登録
                     for picture in tweet.media:
-                        p = Picture(
-                            id=picture.id,
-                            tweet_id=tweet.id,
-                            picture_url=picture.media_url_https
-                        )
+                        p = Picture(id=picture.id, tweet_id=tweet.id, picture_url=picture.media_url_https)
                         p.save()
 
             print(twitter_user.screen_name, 'Done!')
+        print('All tasks Done!')
